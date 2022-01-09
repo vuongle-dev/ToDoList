20ec5a5a8a66fbc66c3b9d4742047c4857fb45d6
vuongle-dev <vuongle.dev@gmail.com>
2022-01-09 22:26:20 +0700

    First upload

---
+++ todo.js
@@ -0,0 +1,17 @@
+// var list = ["a","b","c"];
+// function add() {
+//     let newelement = document.getElementById("input").innerHTML;
+//     list.push(newelement);
+//     renderlist();
+// }
+// function renderlist() {
+//     for (i = 0; i < list.length; i++) {
+        
+//         document.getElementById("list").innerHTML += "<li id=""+"">"+list[i]+"</li>"
+//     }
+// }
+
+// function enterValue(){
+//     var x = document.getElementById('inputValue').innerText
+//     console.log(x)
+// }
